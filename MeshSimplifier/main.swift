6a236361ce50dd2b5b124675cba95865d687bd24
Alexander König <alexander.koenig@namics.com>
2019-12-25 13:54:49 +0100

    Initial Commit

---
+++ MeshSimplifier/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MeshSimplifier
+//
+//  Created by Alexander König on 25.12.19.
+//  Copyright © 2019 Alexander König. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
